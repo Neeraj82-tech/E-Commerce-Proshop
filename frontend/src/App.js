ebd2c4093a9c05e0bf57c20444321f7d0b9640c8
sahil saini <sahil18.official@gmail.com>
2024-07-25 19:58:41 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container } from "react-bootstrap";
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components/Header"
+import Footer from './components/Footer';
+
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+      <Outlet />
+      </Container>
+    </main>
+    <Footer />
+    <ToastContainer />
+    </>
+  )
+};
+
+export default App;
